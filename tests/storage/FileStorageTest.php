800cccefb2d89c8a266da9acff1b25e6dbcb203b
LAV45 <LAV451@gmail.com>
2017-02-27 23:58:49 +0300

    Add test for FileStorage

--- tests/storage/FileStorageTest.php
+++ tests/storage/FileStorageTest.php
@@ -2,6 +2,7 @@
 
 namespace lav45\settings\tests\storage;
 
+use Yii;
 use lav45\settings\storage\FileStorage;
 
 class FileStorageTest extends TestCase
@@ -9,8 +10,26 @@ class FileStorageTest extends TestCase
     protected function getStorage()
     {
         return new FileStorage([
-            'path' => '@runtime/settings',
             'fileMode' => 0644
         ]);
+    }
+
+    public function testGetError()
+    {
+        $storage = $this->getStorage();
+
+        $key = md5(uniqid());
+        $value = 'a:1:{s:13:"template";s:1:"1";}';
+
+        chmod($storage->path, 0444);
+
+        static::assertFalse($storage->setValue($key, $value));
+
+        chmod($storage->path, $storage->dirMode);
+
+        $message = array_pop(Yii::getLogger()->messages);
+
+        static::assertEquals('lav45\settings\storage\FileStorage::setValue', $message[2]);
+        static::assertStringEndsWith('failed to open stream: Permission denied', $message[0]);
     }
 }
